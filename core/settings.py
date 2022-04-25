0d3e244cf6abfa312b2087d18bc890d2cf693ef5
Ruben Hesselink <00302990@rijnijssel.nl>
2022-04-25 14:24:54 +0200

    Version 0.0.3 (SASS)

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third-party apps
+    'django_sass',
 
     # Local apps
     'accounts',
@@ -132,6 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
